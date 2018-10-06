9d21649d73c597f1412f0eebf9907cfb1e9fe56c
Tola Tess <x17131553@student.ncirl.ie>
2018-10-06 19:24:44 +0100

    Intro Scene
    changes to question structure

---
+++ Assets/Scripts/ScriptTextUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTextUI : MonoBehaviour {
+
+    public GameObject dObject;
+    public float wtime;
+
+    void Start()
+    {
+        dObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            dObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(wtime);
+        Destroy(dObject);
+        Destroy(gameObject);
+    }
+}
