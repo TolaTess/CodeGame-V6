978dc884d1980fbf3591c3620e2b12b005a6841e
Tola Tess <x17131553@student.ncirl.ie>
2018-10-06 20:53:39 +0100

    Update to Shooting Script
    JavaTarget1 - code to destroy java object for each answer and NextJavaObject - another to activate collider for next question.

---
+++ Assets/Scripts/NextJavaObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextJavaObject : MonoBehaviour {
+
+    public GameObject prevObject;
+
+    private BoxCollider Target;
+
+    private bool isDead = false;
+
+    void Awake()
+    {
+        Target = gameObject.GetComponentInChildren<BoxCollider>();
+    }
+
+
+    void Update()
+    {
+        if(prevObject == null)
+        {
+           
+                Target.enabled = true;
+        }
+       
+    }
+}
